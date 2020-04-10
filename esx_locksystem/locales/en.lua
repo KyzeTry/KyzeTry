0d3e16a261b18487a7a027c0790d5db679a5f2bc
KyzeTry <63043829+KyzeTry@users.noreply.github.com>
2020-04-10 20:41:32 +0200

    Add files via upload

---
+++ esx_locksystem/locales/en.lua
@@ -0,0 +1,17 @@
+Locales['en'] = {
+    ['vehicle_locked'] = 'Véhicule verrouillé',
+    ['vehicle_unlocked']  = 'Véhicule déverrouillé',
+    ['keys_not_inside'] = 'Les clés ne sont pas à l\'intérieur',
+    ['lock_cooldown'] = 'Vous devez attendre %s secondes',
+    ['vehicle_not_owned'] = 'Ce véhicule n\'est pas le vôtre',
+    ['missing_argument_first'] = 'Premier argument manquant : /givekey <id> <plate>',
+    ['missing_argument_second'] = 'Second argument manquant : /givekey <id> <plate>',
+    ['player_not_found'] = 'Joueur non trouvé',
+    ['cannot_target_yourself'] = 'Vous ne pouvez pas vous cibler',
+    ['vehicle_not_exist'] = 'Le véhicule avec cette plaque n\'existe pas',
+    ['target_has_keys_sender'] = 'La cible a déjà les clés du véhicule',
+    ['target_has_keys_receiver'] = '%s essayé de vous donner ses clés, mais vous les aviez déjà',
+    ['you_gave_keys'] = "Vous avez donné les clés du véhicule %s à %s",
+    ['you_received_keys'] = "On vous a remis les clés du véhicule %s par %s",
+    ['could_not_find_plate'] = "Impossible de trouver la plaque!"
+}
